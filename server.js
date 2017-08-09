87c461c8bb19e31635f41a5937305834554d6521
Vishnu Achanta <30757150+Vishnu-Achanta@users.noreply.github.com>
2017-08-10 00:28:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one', function (req, res) {
+  res.send('article one requested');
+});
+
+app.get('/article-2', function (req, res) {
+  res.send('article 2 requested');
+});
+
+app.get('/article-3', function (req, res) {
+  res.send('article 3 requested');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
