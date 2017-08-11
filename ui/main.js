055163964c4c56ab18c7ac64ee751fba1ff4bedd
Vishnu Achanta <30757150+Vishnu-Achanta@users.noreply.github.com>
2017-08-12 03:42:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,9 @@
 console.log('Loaded!');
+
+var element = document.getElementById('main-text');
+element.innerHTML('It is a secret');
+
+var img = document.getElementById('madi');
+img.onclick = function(){
+     img.style.marginLeft = '100 px'  ;
+};
