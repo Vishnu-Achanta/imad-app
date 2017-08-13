ee9d6cd49965483ca8ce80ad3be19572fbef9f04
Vishnu Achanta <30757150+Vishnu-Achanta@users.noreply.github.com>
2017-08-13 16:28:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,16 +1,21 @@
 console.log('Loaded!');
 
-var element = document.getElementById('main-text');
-element.innerHTML = 'It is a secret' ;
-
-var img = document.getElementById('madi');
-
-var marginLeft = 0;
-
-function moveRight() {
-    marginLeft = marginLeft + '1' ;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function(){
-     var interval = setInterval(moveRight, 500);
+var button = document.getElementById('counter');
+var counter =0;
+button.onclick = function() {
+  
+  //MAke a request to counter endpoint
+  var request= new XMLHttpRequest();
+  
+  request.onreadystatechange = function(){
+    if(request.readystate === XMLHttpRequest.done){
+        if(request.status === 200){
+            var counter = request.responseText;
+            var count = document.getElementById('count');
+            count.innerHTML=counter.toString();
+        }
+    }
+  };
 };
+
+
