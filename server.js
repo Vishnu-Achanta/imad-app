8a90522f8a84c6f436bcda257e87bd780e3d15b8
Vishnu Achanta <30757150+Vishnu-Achanta@users.noreply.github.com>
2017-08-21 03:59:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg');
+
+var config = {
+    user: 'ee150002001',
+    database: 'ee15002001',
+    host: 'db.imad.hasura.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -73,6 +82,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config);
+
+app.get('/test-db', function (req, res) {
+    
+    pool.query('SELECT FROM test', function(err, result){
+       if(err){
+           res.status(500).send(err.toString());
+       } else{
+           res.send(JSON.stringify(result.rows));
+       }
+    });
+  
+});
 var names=[];
 
 app.get('/submit', function (req, res) {
