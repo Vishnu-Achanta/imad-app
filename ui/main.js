5924f963030c499ca516e7a21276c77d4d1dc25a
Vishnu Achanta <30757150+Vishnu-Achanta@users.noreply.github.com>
2017-08-20 23:33:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,15 +26,27 @@ var name = nameInput.value;
 var submit = document.getElementById('sbt_btn');
 submit.onclick = function(){
    //make rquest
-   
+   var request= new XMLHttpRequest();
+  
+  request.onreadystatechange = function(){
+    if(request.readystate === XMLHttpRequest.done){
+        if(request.status === 200){
+            var names = ['name1','name2','name3'];
+            var list='';
+            for(var i=0; i< names.length; i++){
+                list += '<li>' + names[i] + '</li>';
+            }
+            var namelist = document.getElementById('namelist');
+            namelist.innerHTML= list;
+        }
+    }
+  };
+  
+  request.open('GET', 'http://ee150002001.imad.hasura-app.io/submit?name=' , true);
+  request.send('null');
+};
    // capture and render
-   var names = ['name1','name2','name3'];
-   var list='';
-   for(var i=0; i< names.length; i++){
-       list += '<li>' + names[i] + '</li>';
-   }
-   var namelist = document.getElementById('namelist');
-   namelist.innerHTML= list;
+   
 };
 
 
