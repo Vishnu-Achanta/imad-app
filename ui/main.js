9867b5f40c5a4651a9d7a3d763ee87f5ed9cb863
Vishnu Achanta <30757150+Vishnu-Achanta@users.noreply.github.com>
2017-08-20 23:14:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,7 +32,7 @@ submit.onclick = function(){
    var names = ['name1','name2','name3'];
    var list='';
    for(var i=0; i< names.lenght; i++){
-       console.log['for'];
+       console.log[('for');
        list += '<li>' + names[i] + '</li>';
    }
    var namelist = document.getElementById('namelist');
