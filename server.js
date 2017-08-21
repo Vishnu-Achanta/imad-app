bc3ed4a84f78486579bf940225bd89e21c9a28e0
Vishnu Achanta <30757150+Vishnu-Achanta@users.noreply.github.com>
2017-08-21 19:17:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,15 +101,13 @@ var names=[];
 
 app.get('/submit', function (req, res) {
  var name = req.query.name;
- 
  names.push(name);
- 
  res.send(JSON.stringify(names));
 });
 
 app.get('/articles/:articleName', function (req, res) {
  
-    pool.query("SELECT * FROM article where title =' ",+ req.params.articleName + "'", function(err, result){
+    pool.query("SELECT * FROM article WHERE title ='",+ req.params.articleName+"'", function(err, result){
        if(err){
            res.status(500).send(err.toString());
        } else{
