1d7a0d1f692706e4ee056fe6b9f89c9242ba83e6
Vishnu Achanta <30757150+Vishnu-Achanta@users.noreply.github.com>
2017-08-22 02:05:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,13 +62,23 @@ app.get('/', function (req, res) {
 function hash(input, salt){
     
     var hashed= crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
-    return(hashed.toString('hex'));
+    return['pbkdf2', salt, 10000, hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input', function (req, res) {
   var hashedString = hash(req.params.input, 'this-is-a-random-string');
   res.send(hashedString);
 });
+
+app.get('/create-user', function (req, res) {
+    
+    var salt = crypto.GetRandomBytes(128).toString('hex');
+    var dbString = hash(password, salt);
+    
+    
+    
+});
+
 
 var pool = new Pool(config);
 
