849cd66528cd0dbaf68b1a5c80693a1b74546c49
Vishnu Achanta <30757150+Vishnu-Achanta@users.noreply.github.com>
2017-08-22 01:40:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var crypto = requuire('crypto');
 var Pool = require('pg').Pool;
 
 var config = {
@@ -57,6 +57,16 @@ return htmlTemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+function hash(input, salt){
+    
+    var hashed= crypto.pbkdf2Sync(input, salt, 10000, 512, 'sha512');
+}
+
+app.get('/hash/:input', function (req, res) {
+  var hashedString = hash(req.params.input, 'this-is-a-random-string');
+  res.send(hashedString);
 });
 
 var pool = new Pool(config);
