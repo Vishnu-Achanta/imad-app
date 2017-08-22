413057756fe56babb7e69cde40efb12c2ee98dad
Vishnu Achanta <30757150+Vishnu-Achanta@users.noreply.github.com>
2017-08-23 04:13:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -123,39 +123,6 @@ app.post('/login', function (req, res) {
     });
 });
 
-app.post('/enter', function (req, res) {
-    
-    var username = req.body.username;
-    var password = req.body.password;
-    
-        pool.query('SELECT * FROM  "user" WHERE username = $1', [username], function(err, result){
-        if(err)
-        {
-           res.status(500).send(err.toString());
-        }
-        else
-        {
-           if(req.rows.length === 0)
-           {
-               res.status(403).send('Username/password is invalid');
-           } 
-           else
-           {
-               var dbString = result.rows[0].password;
-               var salt = dbString.split('$')[2];
-               var hashedPassword = hash(password, salt);
-               if(hashedPassword === dbString)
-               {
-                   res.send('Credentials are valid');    
-               }
-               else
-               {
-                   res.status(403).send('Password is invalid');
-               }
-           }
-        }
-    });
-});
 
 var pool = new Pool(config);
 
