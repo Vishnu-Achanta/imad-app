dc7916fe4feeedddacbfa760ba6cdf1d79966c1a
Vishnu Achanta <30757150+Vishnu-Achanta@users.noreply.github.com>
2017-08-23 05:00:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -135,6 +135,12 @@ app.get('/check-login', function(req, res){
        res.send('You are not logged in');
    }
 });
+
+app.get('/logout', function(req, res){
+   delete req.session.auth;
+   res.send('You are logged out');
+});
+
 var pool = new Pool(config);
 
 app.get('/test-db', function (req, res) {
