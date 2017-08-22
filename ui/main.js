dd086f33f4ad69fe91c0c0f51e33791a4f37a668
Vishnu Achanta <30757150+Vishnu-Achanta@users.noreply.github.com>
2017-08-23 04:34:35 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -20,7 +20,7 @@ submit.onclick = function(){
   };
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
-  request.open('POST', 'http://ee150002001.imad.hasura-app.io/submit?name='+name , true);
+  request.open('POST', 'http://ee150002001.imad.hasura-app.io/login' , true);
   request.setRequestHeader('Content-Type', 'application/json');
   request.send('JSON.stringify({username: username, password: password})');
 };
